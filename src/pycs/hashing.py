93d6a2ac354aa0176aed440297a738ee9590aed0
Saurabh Sirdeshmukh <saurabh@Saurabhs-MacBook-Pro.local>
2025-05-19 02:27:24 +0530

    - adds objective & outline of the intended dict implementation

---
+++ src/pycs/hashing.py
@@ -0,0 +1,43 @@
+"""
+The objective here is not to implement a substitute for the in-built `dict`. The focus is on how - for a given key,
+we can map it to a unique numeric value. This is important because, the very premise underlying the hash table is that
+we can use the prevailing numeric value as an index into an array where the given key-value pair shall be stored.
+
+It's essential to note that for Python or for that matter any language, the hash table implementation needs to map
+any given key to a unique numeric which shall act as an index into **a finite array within the memory bounds.**
+Which means, a numeric hash function like (or their variations) - FNV, MurmurHash, CityHash, SipHash etc. are used
+internally; typically these functions will produce a large number which again needs to be mapped to a smaller number
+such that it can be an index into finite array of buckets. This is done using the modulo operator.
+
+References:
+    - https://benhoyt.com/writings/hash-table-in-c/
+    - https://craftinginterpreters.com/hash-tables.html
+    - https://en.wikipedia.org/wiki/Non-cryptographic_hash_function
+
+Typical implementation looks like below:
+::
+
+    effective_hash = non_cryptographic_fn_like_fnv_murmur_siphash(key) % num_buckets
+
+For any programming language, in general, string is a reliable & obvious choice for a key as the language will use one
+of the above-mentioned numeric hash functions to generate a unique number for the string. Even for use defined classes
+that implement their own hashing logic by overriding __hash__() in Python or hashCode() in Java, it's still preferable
+to leverage the string field(s) of the class to generate a unique number.
+
+::
+
+    # python
+    >>> s = 'hello pyshell'
+    >>> s.__hash__()
+    >>> -4446671785528942560
+
+    # java
+    jshell> String s = "hello jshell";
+    s ==> "hello jshell"
+
+    jshell> System.out.println(s.hashCode());
+    -586076396
+"""
+
+class PycsDict:
+    pass
