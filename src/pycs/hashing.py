003c4446ffbb2202f80f1a88c9818a1aefb931ad
Saurabh Sirdeshmukh <saurabh@Saurabhs-MacBook-Pro.local>
2025-05-19 02:34:35 +0530

    - updates docs for hashing module

--- src/pycs/hashing.py
+++ src/pycs/hashing.py
@@ -3,11 +3,11 @@ The objective here is not to implement a substitute for the in-built `dict`. The
 we can map it to a unique numeric value. This is important because, the very premise underlying the hash table is that
 we can use the prevailing numeric value as an index into an array where the given key-value pair shall be stored.
 
-It's essential to note that for Python or for that matter any language, the hash table implementation needs to map
-any given key to a unique numeric which shall act as an index into **a finite array within the memory bounds.**
-Which means, a numeric hash function like (or their variations) - FNV, MurmurHash, CityHash, SipHash etc. are used
-internally; typically these functions will produce a large number which again needs to be mapped to a smaller number
-such that it can be an index into finite array of buckets. This is done using the modulo operator.
+It's essential to note that for Python or for that matter any programming language, the hash table implementation needs
+to map any given key to a unique number which shall act as an index into **a finite array within the memory bounds.**
+Which means, a numeric hash function like (or their variations) - FNV, MurmurHash, CityHash, SipHash etc. is used
+internally; typically these functions will produce a large number which is again mapped to a smaller number using the
+modulo operator such that it resolves to an index into finite array of buckets in RAM.
 
 References:
     - https://benhoyt.com/writings/hash-table-in-c/
@@ -20,7 +20,7 @@ Typical implementation looks like below:
     effective_hash = non_cryptographic_fn_like_fnv_murmur_siphash(key) % num_buckets
 
 For any programming language, in general, string is a reliable & obvious choice for a key as the language will use one
-of the above-mentioned numeric hash functions to generate a unique number for the string. Even for use defined classes
+of the above-mentioned numeric hash functions to generate a unique number for the string. Even for user defined classes
 that implement their own hashing logic by overriding __hash__() in Python or hashCode() in Java, it's still preferable
 to leverage the string field(s) of the class to generate a unique number.
 
